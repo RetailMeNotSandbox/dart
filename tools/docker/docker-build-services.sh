408258ca8af8b6fa762c5ee335b2dc1124fe15e5
Za Irteza <mirteza@rmn.com>
2017-08-16 09:49:40 -0500

    fix s3 engine metadata.py. remove engine and sub worker from docker build/push scripts

--- tools/docker/docker-build-services.sh
+++ tools/docker/docker-build-services.sh
@@ -12,16 +12,12 @@ OLD_IFS=${IFS}
 IFS=
 CONFIG="$(DART_ROLE=tool dart_conf)"
 DOCKER_IMAGE_FLASK=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.web.boto_args.Parameters[@.ParameterKey is 'FlaskWorkerDockerImage'][0].ParameterValue")
-DOCKER_IMAGE_WORKER_ENGINE=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.'engine-worker'.boto_args.Parameters[@.ParameterKey is 'EngineWorkerDockerImage'][0].ParameterValue")
 DOCKER_IMAGE_WORKER_TRIGGER=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.'trigger-worker'.boto_args.Parameters[@.ParameterKey is 'TriggerWorkerDockerImage'][0].ParameterValue")
-DOCKER_IMAGE_WORKER_SUBSCRIPTION=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.'subscription-worker'.boto_args.Parameters[@.ParameterKey is 'SubscriptionWorkerDockerImage'][0].ParameterValue")
 IFS=${OLD_IFS}
 
 set -x
 docker build -f tools/docker/Dockerfile-flask               -t ${DOCKER_IMAGE_FLASK} .
-docker build -f tools/docker/Dockerfile-engine-worker       -t ${DOCKER_IMAGE_WORKER_ENGINE} .
 docker build -f tools/docker/Dockerfile-trigger-worker      -t ${DOCKER_IMAGE_WORKER_TRIGGER} .
-docker build -f tools/docker/Dockerfile-subscription-worker -t ${DOCKER_IMAGE_WORKER_SUBSCRIPTION} .
 set +x
 
 REMOVABLE=$(docker images -f "dangling=true" -q)
--- tools/docker/docker-push-services.sh
+++ tools/docker/docker-push-services.sh
@@ -13,17 +13,13 @@ OLD_IFS=${IFS}
 IFS=
 CONFIG="$(DART_ROLE=tool dart_conf)"
 DOCKER_IMAGE_FLASK=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.web.boto_args.Parameters[@.ParameterKey is 'FlaskWorkerDockerImage'][0].ParameterValue")
-DOCKER_IMAGE_WORKER_ENGINE=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.'engine-worker'.boto_args.Parameters[@.ParameterKey is 'EngineWorkerDockerImage'][0].ParameterValue")
 DOCKER_IMAGE_WORKER_TRIGGER=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.'trigger-worker'.boto_args.Parameters[@.ParameterKey is 'TriggerWorkerDockerImage'][0].ParameterValue")
-DOCKER_IMAGE_WORKER_SUBSCRIPTION=$(dart_conf_value "${CONFIG}" "$.cloudformation_stacks.'subscription-worker'.boto_args.Parameters[@.ParameterKey is 'SubscriptionWorkerDockerImage'][0].ParameterValue")
 IFS=${OLD_IFS}
 
 $(aws ecr get-login --registry-ids $ECR_ID)
 set -x
 docker push ${DOCKER_IMAGE_FLASK}
-docker push ${DOCKER_IMAGE_WORKER_ENGINE}
 docker push ${DOCKER_IMAGE_WORKER_TRIGGER}
-docker push ${DOCKER_IMAGE_WORKER_SUBSCRIPTION}
 set +x
 
 
